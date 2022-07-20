668ca27f044b41b8e279a1c324a2baaa31d9d56e
tung3122002 <tung3122002@gmail.com>
2022-07-20 23:24:24 +0700

    feact : fix header

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,10 @@ import Image from 'next/image'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFaceRelieved } from '@fortawesome/pro-solid-svg-icons'
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-         <FontAwesomeIcon icon={faFaceRelieved} />
       <Header/>
       <Footer/>
     </div>
